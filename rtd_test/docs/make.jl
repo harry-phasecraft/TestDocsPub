281d085cd776410fbf93553c7096860fb6616531
harry-phasecraft <harry@phasecraft.io>
2025-08-07 13:11:52 +0100

    trial julia output

---
+++ rtd_test/docs/make.jl
@@ -0,0 +1,3 @@
+using Documenter, rtd_test
+
+makedocs(sitename="TESTEST")
---
+++ rtd_test/src/rtd_test.jl
@@ -0,0 +1,13 @@
+"""
+Adding some documentation
+
+"""
+
+module rtd_test
+
+"""
+documenting this function
+"""
+greet() = print("Hello World!")
+
+end # module rtd_test
